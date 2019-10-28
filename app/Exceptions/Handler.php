af2fc4606105cf6fd6f757059828eabcd201cfb7
dev4 <dev1@shayansolutions.com>
2019-10-28 17:29:30 +0500

    improved message for SessionExpired exception

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -64,7 +64,7 @@ class Handler extends ExceptionHandler
         }
 
         if ($e instanceof SessionExpired) {
-            return response()->json(['status' => 'error', 'message' => 'Session Expired']);
+            return response()->json(['status' => 'error', 'message' => 'Session is already booked!']);
         }
 
         if ($e instanceof SessionBookedStartedOrEnded) {
