c5b8a90b9654179437cf0ef162a89777d02f83e3
dev4 <dev1@shayansolutions.com>
2019-11-13 20:30:30 +0500

    added ApplicationSettingsSeeder to DBSeeder run method

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -13,7 +13,8 @@ class DatabaseSeeder extends Seeder
     {
         //$this->call(RolesTableSeeder::class);
         //$this->call(UsersTableSeeder::class);
-        $this->call(UsersProfileSeerder::class);
+//        $this->call(UsersProfileSeerder::class);
 
+        $this->call(ApplicationSettingsSeeder::class);
     }
 }
