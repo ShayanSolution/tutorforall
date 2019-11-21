099b785c5d9c5ce891efd4d08154b409bab9a7e1
Arslan <shayansolutions@gmail.com>
2019-11-21 17:06:29 +0500

    adding service layer

---
+++ app/Services/ApplyPeakFactor.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: HP
+ * Date: 11/21/2019
+ * Time: 4:35 PM
+ */
+
+namespace App\Services;
+
+
+class ApplyPeakFactor
+{
+    public function __construct(){
+
+    }
+
+    public function execute(){
+
+    }
+}
---
+++ app/Services/CostCalculation/CategoryCost.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: HP
+ * Date: 11/21/2019
+ * Time: 5:01 PM
+ */
+
+namespace App\Services\CostCalculation;
+
+
+class CategoryCost
+{
+    public function __construct()
+    {
+    }
+
+    public function execute($categoryId){
+
+    }
+
+}
