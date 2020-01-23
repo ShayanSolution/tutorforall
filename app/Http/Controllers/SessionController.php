0f79433a047e702d86000e1605c37c93931822cf
Dev1 <dev1@shayansolutions.com>
2020-01-23 13:49:44 +0500

    fix get latest session api

--- app/Http/Controllers/SessionController.php
+++ app/Http/Controllers/SessionController.php
@@ -606,9 +606,9 @@ class SessionController extends Controller
         $data = [];
         if($roleId == 2){
             $session = Session::where('tutor_id', $userId)->with('tutor','student')->orderBy('updated_at', 'desc')->first();
-            $tutorPhone = User::where('id', $session->tutor_id)->first();
             if($session){
                 $rating = Rating::where('session_id', $session->id)->first();
+                $tutorPhone = User::where('id', $session->tutor_id)->first();
             }else{
                 return response()->json(
                     [
