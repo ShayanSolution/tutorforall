e79a7344550612711d65b4bb71d3d61dd9416987
Dev1 <dev1@shayansolutions.com>
2020-02-03 13:27:35 +0500

    classes and subjects in proper formate

--- app/Models/ProgramSubject.php
+++ app/Models/ProgramSubject.php
@@ -20,12 +20,17 @@ class ProgramSubject extends Model
 
     public function getSubjectsDetail($userId)
     {
-        $subjects = '';
+        $subjects = [];
         $programSubjects = $this->where('user_id', $userId)->with('program', 'subject')->get();
         foreach ($programSubjects as $programSubject){
-            $subjects .= $programSubject->program->name.' - '.$programSubject->subject->name.', ';
+            if (!key_exists($programSubject->program->name, $subjects))
+                $subjects[$programSubject->program->name] = '';
+
+            $isComma =  !empty($subjects[$programSubject->program->name]) ? $subjects[$programSubject->program->name].', ' : $subjects[$programSubject->program->name];
+
+            $subjects[$programSubject->program->name] = $isComma.$programSubject->subject->name;
         }
-        return $string = rtrim($subjects, ', ');
+        return $string = $subjects;
     }
 
 
