b7bca97455784a468b0cff78a087d5ba896e9eb7
Dev1 <dev1@shayansolutions.com>
2020-02-03 14:51:00 +0500

    classes and subjects in proper format revert and release when M3 released

--- app/Models/ProgramSubject.php
+++ app/Models/ProgramSubject.php
@@ -20,18 +20,24 @@ class ProgramSubject extends Model
 
     public function getSubjectsDetail($userId)
     {
-        $subjects = [];
+        $subjects = '';
         $programSubjects = $this->where('user_id', $userId)->with('program', 'subject')->get();
         foreach ($programSubjects as $programSubject){
-            if (!key_exists($programSubject->program->name, $subjects))
-                $subjects[$programSubject->program->name] = '';
-
-            $isComma =  !empty($subjects[$programSubject->program->name]) ? $subjects[$programSubject->program->name].', ' : $subjects[$programSubject->program->name];
-
-            $subjects[$programSubject->program->name] = $isComma.$programSubject->subject->name;
+            $subjects .= $programSubject->program->name.' - '.$programSubject->subject->name.', ';
         }
-        return $string = $subjects;
+        return $string = rtrim($subjects, ', ');
+
+        // @todo after M-3 release uper code comment and down code uncomment
+//        $subjects = [];
+//        $programSubjects = $this->where('user_id', $userId)->with('program', 'subject')->get();
+//        foreach ($programSubjects as $programSubject){
+//            if (!key_exists($programSubject->program->name, $subjects))
+//                $subjects[$programSubject->program->name] = '';
+//
+//            $isComma =  !empty($subjects[$programSubject->program->name]) ? $subjects[$programSubject->program->name].', ' : $subjects[$programSubject->program->name];
+//
+//            $subjects[$programSubject->program->name] = $isComma.$programSubject->subject->name;
+//        }
+//        return $string = $subjects;
     }
-
-
 }
