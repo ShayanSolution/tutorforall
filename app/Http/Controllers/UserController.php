39c96f41fdb1f2c3436f08461bb1139ba641ba30
Dev1 <dev1@shayansolutions.com>
2021-02-03 13:26:17 +0500

    fix demostartedat null at online

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -849,7 +849,7 @@ class UserController extends Controller
                 $getLastSession = Session::where('tutor_id', $user_id)->where(function ($query) {
                     $query->where('cancelled_from', 'tutor')
                         ->orWhere('cancelled_from', 'student');
-                })->whereNotNull('cancelled_by')->orderBy('id', 'desc')->first();
+                })->whereNull('demo_started_at')->orderBy('id', 'desc')->first();
                 if ($getLastSession) {
                     $now  = Carbon::now();
                     $date = Carbon::make($getLastSession->created_at);
