2cf9bba55542b91c5f45a65c3ad973e577c64487
Dev1 <dev1@shayansolutions.com>
2021-02-12 17:01:52 +0500

    auto connected send tutor notification

---
+++ app/Jobs/SendSessionConnectedNotification.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Helpers\Push;
+use App\Models\User;
+use Illuminate\Support\Facades\Config;
+use Log;
+
+class SendSessionConnectedNotification extends Job {
+
+	protected $userId;
+	protected $message;
+
+	/**
+	 * Create a new job instance.
+	 *
+	 * @return void
+	 */
+	public function __construct($userId, $message) {
+		$this->userId = $userId;
+		$this->message = $message;
+	}
+
+	/**
+	 * Execute the job.
+	 *
+	 * @return void
+	 */
+	public function handle() {
+        $userId = $this->userId;
+        //get tutor device token to send notification
+        $user = User::where('id','=', $userId)->first();
+		if (!empty($user->device_token)) {
+			Log::info('Sending notification session Connected'.$this->message);
+			$title      = Config::get('user-constants.APP_NAME');
+			$body       = $this->message;
+			$customData = array(
+				'notification_type' => 'offline_notification',
+			);
+			Push::handle($title, $body, $customData, $user);
+		}
+	}
+}
