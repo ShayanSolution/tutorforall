b95218f9a0818362c880d7d61229070879c65b91
Dev1 <dev1@shayansolutions.com>
2021-02-17 16:39:14 +0500

    trying to fix DB time zone by using excesser

--- app/Models/Notification.php
+++ app/Models/Notification.php
@@ -3,9 +3,17 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Carbon;
 
 class Notification extends Model
 {
+    public function getCreatedAtAttribute($value)
+    {
+        return Carbon::createFromTimestamp(strtotime($value))
+            ->timezone(env('APP_TIMEZONE'))
+            ->toDateTimeString()
+            ;
+    }
     public function notification_status(){
         return $this->hasMany(NotificationStatus::class);
     }
