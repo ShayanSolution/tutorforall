61132e5d00844be69e12d5ab75d95b2a96793036
FullStackDev <dev1@shayansolutions.com>
2021-03-10 16:17:16 +0500

    fix created at time formate issue

--- app/Wallet.php
+++ app/Wallet.php
@@ -3,8 +3,15 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Carbon;
 
 class Wallet extends Model
 {
-    //
+    public function getCreatedAtAttribute($value)
+    {
+        return Carbon::createFromTimestamp(strtotime($value))
+            ->timezone(env('APP_TIMEZONE'))
+            ->toDateTimeString()
+            ;
+    }
 }
